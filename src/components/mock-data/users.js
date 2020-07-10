a1eeee767de9960526a85365a7441d3bb146c9f6
Christian Corrales <ccorr066@fiu.edu>
2020-07-10 03:37:15 -0700

    added data

---
+++ src/components/mock-data/users.js
@@ -0,0 +1,14 @@
+const users = [
+  {
+    id: "e838cec2-1a0b-4249-8f5d-96f9ba6c0894",
+    email: "christianC1026@gmail.com",
+    password: "5F4DCC3B5AA765D61D8327DEB882CF99",
+    createdAt: 1594331721,
+  },
+  {
+    id: "c55f0adf-f1c5-48e5-a714-966bb96e3d4e",
+    email: "whatdoyouwant@yahoo.com",
+    password: "2B40AAA979727C43411C305540BBED50",
+    createdAt: 1581561813,
+  },
+]
